e1a0b5844c397f4d64c5f8e59eced4a7c22beb7b
Roo-rabab <rababmchich312@gmail.com>
2023-11-16 10:57:27 -0800

    0x01.fix_my_code

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-""" User_class"""
+"""
+User class
+"""
 
 class User():
-    """Documents."""
+    """ Documentation """
 
     def __init__(self):
-        """Documents. """
+        """ Documentation """
         self.__email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documents."""
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
 
     @property
     def email(self):
-        """Documents."""
+        """ Documentation """
         return self.__email
-   
+
+    @email.setter
+    def email(self, value):
+        """ Documentation """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
     
 if __name__ == "__main__":
-
     u = User()
-    u.email = "shametlab@gmail.com"
+    u.email = "john@snow.com"
     print(u.email)
